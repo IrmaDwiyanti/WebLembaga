b798588050662f99cf1198ac0ded5658f541fcf4
IrmaDwiyanti <irmadwiyanti.sky.@gmail.com>
2024-10-06 23:31:15 +0700

    header

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -4,10 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Lembaga UIN SGD Bandung</title>
 </head>
 <body>
-    <h1>halaman tes</h1>
-    <h2>halam 2</h2>
+    <header>
+        <div class="header-container">
+            <div class="contact-info">
+                <span>@uinsgd.official</span>
+                <span>(0221) 7800525</span>
+                <span>8@gmail.com</span>
+            </div>
+        </div>
+    </header>
 </body>
 </html>
