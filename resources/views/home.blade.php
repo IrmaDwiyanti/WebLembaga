126877442837d5bd3ec0077d373bf909d5916284
IrmaDwiyanti <irmadwiyanti.sky.@gmail.com>
2024-10-08 13:21:13 +0700

    home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UIN Sunan Gunung Djati Bandung</title>
+    <link href="https://fonts.googleapis.com/css2?family=Libre+Caslon+Text:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+    <header>
+        <div class="header-container">
+            <div class="contact-info">
+                <span>@uinsgd.official</span>
+                <span>(0221) 7800525</span>
+                <span>info@uinsgd.ac.id</span>
+            </div>
+        </div>
+    </header>
+
+    <section class="hero">
+        <h1>LEMBAGA DAN UNIT</h1>
+        <h2>UIN SUNAN GUNUNG</h2>
+        <h3>DJATI BANDUNG</h3>
+    </section>
+
+    <section class="profile">
+        <div class="cards-container">
+            <div class="card">
+                <h3>Visi dan Misi</h3>
+                <p>Menjadikan Universitas Islam Negeri yang unggul, kompetitif, dan inovatif berbasis Al-Quran & Al-Hadist. Selalu melakukan pengembangan Ilmu Pengetahuan berbasis Keislaman untuk memberikan kontribusi bagi masyarakat.</p>
+                <a href="#">Learn More</a>
+            </div>
+            <div class="card">
+                <h3>Sejarah</h3>
+                <p>Sejarah berdirinya Universitas Islam Negeri Sunan Gunung Djati Bandung tidak terlepas dari peran para ulama dan tokoh masyarakat. Kampus ini telah berkembang pesat sejak berdirinya di tahun 1968 dan berkomitmen pada pendidikan berbasis Islam.</p>
+                <a href="#">Learn More</a>
+            </div>
+            <div class="card">
+                <h3>Tujuan</h3>
+                <p>Universitas berkomitmen untuk menyiapkan lulusan yang berakhlak mulia, berbasis ilmu, dan inovatif. Dengan misi untuk membangun SDM yang kompeten dalam bidang ilmu pengetahuan, teknologi, dan seni berdasarkan Islam.</p>
+                <a href="#">Learn More</a>
+            </div>
+        </div>
+    </section>
+
+    <section id="lembaga1" class="section-p1">
+        <h2>Lembaga-lembaga</h2>
+        <p>UIN Sunan Gunung Djati Bandung</p>
+
+        <div class="container">
+            <div class="card">
+                <img src="images/lp2m.jpg" alt="Lembaga Penelitian dan Pengabdian Masyarakat">
+                <a href="#">
+                  <div class="label">Lembaga Penelitian dan Pengabdian Masyarakat</div>
+                </a>
+            </div>
+            <div class="card">
+                <img src="images/Perpustakaan.jpg" healt="Lembaga Peminjaman Mutu" height="auto">
+                <div class="label">Lembaga Peminjaman <br> Mutu</div>
+            </div>
+            <div class="card">
+                <img src="images/lab.jpg" alt="Bagian Kerjasama dan Pengembangan Lembaga">
+                <div class="label">Bagian Kerjasama dan Pengembangan Lembaga</div>
+            </div>
+            <div class="card">
+                <img src="images/mahasiswa.jpg" alt="Lembaga Kemahasiswaan dan Alumni">
+                <div class="label">Lembaga Kemahasiswaan dan Alumni</div>
+            </div>
+        </div>
+    </section>
+
+    <div class="video-container">
+      <div class="background"></div>
+      <iframe width="560" height="315" 
+          src="https://www.youtube.com/embed/yprwfSH4h9c?si=tGxRilaSKT-Px30E" 
+          title="YouTube video player" 
+          frameborder="0" 
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+          referrerpolicy="strict-origin-when-cross-origin" 
+          allowfullscreen>
+      </iframe>
+  </div>
+
+  <section class="berita-pembaruan">
+    <div class="container-berita">
+      <h2>News & Updates</h2>
+      <div class="grid-berita">
+        <div class="gambar-berita">
+          <img src="images/3.jpeg" alt="Gedung Universitas">
+        </div>
+        <div class="daftar-berita">
+          <div class="item-berita">
+            <span class="tanggal-berita">15 Juni 2024</span>
+            <p>Kegiatan 10 Jurusan dengan UKT Termurah di UIN Bandung untuk Jalur SNBP, SNBT, dan Mandiri</p>
+          </div>
+          <div class="item-berita">
+            <span class="tanggal-berita">26 September 2024</span>
+            <p>Urgensi dan Problematika Moderasi Beragama di Kalangan Milenial</p>
+          </div>
+          <div class="item-berita">
+            <span class="tanggal-berita">20 April 2022</span>
+            <p>Studium Generale Akuntansi Syariah untuk Mahasiswa Baru: Kenali, Pahami, dan Siap Berprofesi</p>
+          </div>
+          <button class="muat-lebih">Muat Lebih Banyak</button>
+        </div>
+      </div>
+    </div>
+  </section>
+  
+  <footer>
+    <div class="footer-container">
+      <div class="footer-logo">
+        <img src="images/logo.png" alt="Logo UIN">
+      </div>
+      <div class="footer-info">
+        <p>Jl. A.H. Nasution No. 105, Cibiru, Bandung 40614</p>
+        <p>© 2024 - UIN SGD Bandung</p>
+      </div>
+    </div>
+  </footer>
+
+</body>
+</html>
