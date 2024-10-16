c5dd5de02966555f8f7592d5d7327f325329316d
dailfhiana <dailfhianafujianti@gmail.com>
2024-10-16 16:33:44 +0700

    (Revisi Navbar&Footer)

--- resources/views/Lembaga.blade.php
+++ resources/views/Lembaga.blade.php
@@ -1,19 +1,41 @@
 <!DOCTYPE html>
 <html lang="id">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Lembaga - UIN SGD Bandung</title>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UIN Sunan Gunung Djati Bandung</title>
+    <link href="https://fonts.googleapis.com/css2?family=Libre+Caslon+Text:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    
     <link rel="stylesheet" href="css/Lembaga.css">
-  </head>
-  <body>
-    <header>
-      <div class="header-info">
-        <span>Email: uinsgd@official.com</span>
-        <span>Telepon: (022) 7800525</span>
-        <span>Mail: UIN SGD Bandung</span>
-      </div>
-    </header>
+</head>
+<body>
+
+<body> 
+  <section class="header">
+    <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">UIN SGD BANDUNG</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            </ul>
+            <div class="navbar-nav">
+              <a class="nav-link active" href="#Profile">LEMBAGA</a>
+              <a class="nav-item nav-link" href="#About">UNIT</a>
+              <a class="nav-item nav-link" href="#Portfolio">UKM</a>
+              <a class="nav-item nav-link" href="#Contact">PRODI</a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  </section>
+</body>
 
     <main>
       <section class="intro">
@@ -67,16 +89,27 @@
       </div>
     </main>
 
-    <footer>
-      <div class="footer-container">
-        <div class="footer-logo">
-          <img src="images/logo.png" alt="Logo UIN" />
+    <footer class="footer">
+        <div class="footer-left">
+            <img src="images/logo.png" alt="Logo UIN">
         </div>
-        <div class="footer-info">
-          <p>Jl. A.H. Nasution No. 105, Cibiru, Bandung 40614</p>
-          <p>© 2024 - UIN SGD Bandung</p>
+      
+        <div class="footer-center">
+            <p>Jl. A.H. Nasution No. 105, <br> Cibiru, Bandung 40614</p>
+            <p class="footer-copyright">&copy; 2024 - UIN SGD Bandung</p>
         </div>
-      </div>
+      
+        <div class="footer-right">
+            <p class="footer-about">Sosial Media</p>
+            <div class="social-icons">
+                <a href="#"><i class="fab fa-facebook"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+                <a href="#"><i class="fab fa-youtube"></i></a>
+            </div>
+            <p>Email: <a>info@uinsgd.ac.id</a></p>
+            <p>(022)7800525</p>
+        </div>
     </footer>
   </body>
 </html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>UIN Sunan Gunung Djati Bandung</title>
     <link href="https://fonts.googleapis.com/css2?family=Libre+Caslon+Text:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
-    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 
-  <nav class="navbar" style="background-color: #2FAA2F;">
-    
-  </nav>      
+<body> 
+  <section class="header">
+    <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">UIN SGD BANDUNG</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            </ul>
+            <div class="navbar-nav">
+              <a class="nav-link active" href="#Profile">LEMBAGA</a>
+              <a class="nav-item nav-link" href="#About">UNIT</a>
+              <a class="nav-item nav-link" href="#Portfolio">UKM</a>
+              <a class="nav-item nav-link" href="#Contact">PRODI</a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  </section>
+</body>
+
 
     
 
@@ -108,17 +130,35 @@
     </div>
   </section>
   
-  <footer>
-    <div class="footer-container">
-      <div class="footer-logo">
-        <img src="images/logo.png" alt="Logo UIN">
-      </div>
-      <div class="footer-info">
-        <p>Jl. A.H. Nasution No. 105, Cibiru, Bandung 40614</p>
-        <p>© 2024 - UIN SGD Bandung</p>
-      </div>
-    </div>
-  </footer>
+  <footer class="footer">
+        <div class="footer-left">
+            <img src="images/logo.png" alt="Logo UIN">
+        </div>
+      
+        <div class="footer-center">
+            <p>Jl. A.H. Nasution No. 105, <br> Cibiru, Bandung 40614</p>
+            <p class="footer-copyright">&copy; 2024 - UIN SGD Bandung</p>
+        </div>
+      
+        <div class="footer-right">
+            <p class="footer-about">Sosial Media</p>
+            <div class="social-icons">
+                <a href="#"><i class="fab fa-facebook"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+                <a href="#"><i class="fab fa-youtube"></i></a>
+            </div>
+            <p>Email: <a>info@uinsgd.ac.id</a></p>
+            <p>(022)7800525</p>
+        </div>
+    </footer>
+
+</body>
+</html>
+  
+
+</body>
+</html>
 
 </body>
 </html>
